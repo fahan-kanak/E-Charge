afdb902bba67980877fec6fa6ea87b0cf190838b
farhan-kanak <farhankanak@gmail.com>
2017-05-06 12:07:58 +0600

    First commit

--- data_retrive.php
+++ data_retrive.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "password");
+    $con = mysqli_connect("localhost", "e_charge", "e_charge");
     if (!$con) {
         exit('Connect Error (' . mysqli_connect_errno() . ') '
                 . mysqli_connect_error());
@@ -7,7 +7,7 @@
     //set the default client character set 
     mysqli_set_charset($con, 'utf-8');
 
-    mysqli_select_db($con, "E_charge");
+    mysqli_select_db($con, "e_charge");
     $requests = mysqli_query($con, "SELECT mobile_no, amount FROM requests");
     while ($row = mysqli_fetch_assoc($requests)) {
         $new_array[] = $row;
